4b78cc0a79c8bf0385a7b7044413fe6a31376deb
HFeeney <haydenbfeeney@gmail.com>
2020-01-13 19:18:25 -0800

    Added to Velocity Calculation to PIDHood

--- src/main/java/frc/robot/commands/PIDHood.java
+++ src/main/java/frc/robot/commands/PIDHood.java
@@ -9,40 +9,47 @@ package frc.robot.commands;
 
 import edu.wpi.first.wpilibj.controller.PIDController;
 import edu.wpi.first.wpilibj2.command.CommandBase;
-import frc.robot.subsystems.TurretSub;
+import frc.robot.subsystems.HoodSub;
 
-public class PIDTurret extends CommandBase {
+public class PIDHood extends CommandBase {
 
   PIDController pidController;
   private final double kP = 0;
   private final double kI = 0;
   private final double kD = 0;
   private final double tolerance = 0;
-  private final double setpoint = 0;
+
+  private final double hoodHeight = 2;
+  private final double gravity = -32.2;
+
+  
 
 
   /**
-   * Creates a new PIDTurret.
+   * Creates a new PIDHood.
    */
-  public PIDTurret(TurretSub subsystem) {
-    // Use addRequirements() here to declare subsystem dependencies.
+  public PIDHood(HoodSub subsystem) {
+    // Use addRequirements () here to declare subsystem dependencies.
     addRequirements(subsystem);
   }
 
   // Called when the command is initially scheduled.
   @Override
   public void initialize() {
-    pidController = new PIDController(kP, kI, kD);
-    pidController.disableContinuousInput();
-    pidController.setTolerance(tolerance); 
+    pidController = new PIDController(multp, multi, multd);
+    pidController.disableContinuousInput(); // if subsystem is rotational, or has a minimum and maximum at the same points, should enableContinuousInput();
+    pidController.setTolerance(tolerance); // the deadband, or margin of error acceptable
     pidController.reset();
+
+    private double distance;
+    private final double a = 8.1875 - hoodHeight;
+    double vel = Math.sqrt((-(Math.pow(d, 2) * q)/Math.pow(a, 2) +  2 * (gravity*Math.pow(d, 2) / 2) / a - 4*gravity*a) / 2);
+    // private final double vel = Math.sqrt( (Math.sqrt( Math.pow(d, 2) * q *( Math.pow(d, 2) * q - 4 * a * r) ) / (Math.pow(a,2) - Math.pow(d, 2) * q)) - ( Math.pow(d, 2) * q) / (Math.pow(a, 2)) + ((2 * r) / (a)) - 2 * q) / Math.sqrt(2);
   }
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
-    double output = pidController.calculate(TurretSub.turretMotor.getSensorCollection().getQuadraturePosition(), setpoint);
-    TurretSub.rotate(output);
   }
 
   // Called once the command ends or is interrupted.
