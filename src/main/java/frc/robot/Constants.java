76b63c42a8571584af5e269b717db03bcea9ef46
Jacob-Seyla <you@example.com>
2020-01-22 13:11:40 -0800

    color senser

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -18,6 +18,15 @@ import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 I2C.Port i2cPortRoboRio = I2C.Port.kOnboard;
 colorSensorV3 l_colorSensor = new ColorSensorV3(i2cPortRoboRio);
 
+Color kBlueTarget = ColorMatch.makeColor(0.143, 0.427, 0.429);
+Color kRedTarget = ColorMatch.makeColor(0.429, 0.143, 0.143);
+Color kGreenTarget = ColorMatch.makeColor();
+Color kYellowTarget = ColorMatch.makeColor();
+
+Color colorSeen = l.colorSensor.getColor();
+
+ColorMatchResult <RandomName> = <SensorName>.matchClosestColor(WhatsBeingDetected);
+
 /**
  * The Constants class provides a convenient place for teams to hold robot-wide
  * numerical or boolean constants. This class should not be used for any other
